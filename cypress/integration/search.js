923b69725191c247cf2400597250c30d3f165997
Johan <javier0218@gmail.com>
2021-07-02 18:47:15 -0400

    Solved T-1 and T-2

---
+++ cypress/integration/search.js
@@ -0,0 +1,31 @@
+
+describe('Search elements', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    })
+    it('Search for elements with multiple results', () => {
+        cy.fixture('index').then((index) => {
+            cy.get(index.searchBox).type('dress');
+            cy.get(index.searchButton).click();
+        })
+        cy.fixture('searchResult').then((searchResult) => {
+            cy.get(searchResult.tittle).should('contain', 'dress');
+        })
+    })
+
+    it('search for elements with no results', () => {
+       cy.fixture('index').then((index)=> {
+           cy.get(index.searchBox).type('qwerty');
+           cy.get(index.searchButton).click();
+       })
+       cy.fixture('searchResult').then((searchResult)=>{
+           cy.get(searchResult.alert).should('contain','No results were found for your search')
+
+       })
+      
+
+    })
+
+})
+
+
